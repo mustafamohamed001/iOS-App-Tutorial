6dbd6b960b4468d1690a73f2bcf3ef1b94220d1b
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2019-12-22 12:23:44 -0500

    Initial Commit

---
+++ First iOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First iOS App
+//
+//  Created by Mustafa Mohamed on 12/22/19.
+//  Copyright © 2019 Mustafa Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
